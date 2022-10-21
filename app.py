421a37bebc2d9ea6638711579b3afab855b226ae
jack <jack.xin88888@gmail.com>
2022-10-21 14:24:24 +0200

    docker2

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ async def predicting(data: Data1):
     # return JSONResponse(content=json_compatible_item_data)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
