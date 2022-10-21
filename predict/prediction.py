ad3060fcb2456d9418e1ffbd2716d6fcbd47a54f
jack <jack.xin88888@gmail.com>
2022-10-21 15:00:19 +0200

    docker4

--- predict/prediction.py
+++ predict/prediction.py
@@ -2,16 +2,14 @@ from array import array
 import numpy
 import joblib
 
+# with open("immoElisa.pkl", "rb") as f:  # working for python and fastAPI
+with open("predict/immoElisa.pkl", "rb") as f:  # working for render with dockerfile
+    model = joblib.load(f)
+
 
 def predict(X_predict: array):
 
-    # with open("immoElisa.pkl", "rb") as f:  # working for python and fastAPI
-    with open("predict/immoElisa.pkl", "rb") as f:  # working for render with dockerfile
-        model = joblib.load(f)
-
-    y_predict = model.predict(X_predict)
-    y_predict = y_predict[0]
-    y_predict = y_predict[0]
+    y_predict = model.predict(X_predict)[0][0]
     return y_predict
 
 
